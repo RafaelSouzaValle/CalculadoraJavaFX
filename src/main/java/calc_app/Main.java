78779efaa064e35815ce313268dd94240d32a697
RafaelSouzaValle <rafaelsouzavalle@gmail.com>
2018-08-07 20:52:42 -0300

    Adicione comentários a Main class
    Comentários descritivos sobre o funcionamento da classe principal.

--- src/main/java/calc_app/Main.java
+++ src/main/java/calc_app/Main.java
@@ -6,16 +6,38 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+/**
+ * Aplicação Calculadora JavaFX
+ * 
+ * @author Rafael.Valle
+ *
+ */
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		//Carrega o Layout contido no arquivo *.fxml 
 		Pane root = FXMLLoader.load(getClass().getResource("/Layout.fxml"));
+		
+		//Cria a cena exibida pela classe principal
 		Scene scene = new Scene(root, 220, 220);
+		
+		//Carrega o Estilo css da aplicação 
 		scene.getStylesheets().add("/style.css");
+		
+		//Configura a cena para ser exibida na tela principal
 		primaryStage.setScene(scene);
+		
+		//Configura o texto da janela da aplicação na barra de título
 		primaryStage.setTitle("Calculadora App");
+		
+		//Impede o redimensionamento da janela
+		primaryStage.setResizable(false);
+		
+		//Exibe a tela principal (palco principal)
 		primaryStage.show();
+		
 	}
 
 	public static void main(String[] args) {
